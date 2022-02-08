5036dd7b6458d5d387c6e0ccb2f9d172f2c0fec8
Prinok7 <Prinok2015@gmail.com>
2022-02-08 21:39:48 +0100

    Adding all files

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x37cE4feA99681e6810cC60AAC64b6b71a919faD2";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-learnweb3-prinok7.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
